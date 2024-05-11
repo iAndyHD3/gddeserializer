2cbe850c449424505d9aaabc00626b3487a85fcc
iAndyHD3 <marcaraus@gmail.com>
2024-05-11 02:44:26 +0200

    move ts to its own folder

--- all.bat
+++ all.bat
@@ -7,17 +7,18 @@ pushd transpiled
 mkdir cpp
 mkdir java
 mkdir c
+mkdir ts
 
 set file=../GJGameLevel.fu
 fut -l java -o java/GJGameLevel.java %file% -n gdd
 fut -l cpp -o cpp/GJGameLevel.cpp %file% -n gdd
 fut -l c -o c/GJGameLevel.c %file% -n gdd
+fut -l ts -o ts/GJGameLevel.ts %file% -n gdd
+fut -l d.ts -o ts/GJGameLevel.d.ts %file% -n gdd
 
 fut -l cs -o GJGameLevel.cs %file% -n gdd
 fut -l d -o GJGameLevel.d %file% -n gdd
 fut -l js -o GJGameLevel.mjs %file% -n gdd
-fut -l ts -o GJGameLevel.ts %file% -n gdd
-fut -l d.ts -o GJGameLevel.d.ts %file% -n gdd
 fut -l swift -o GJGameLevel.swift %file% -n gdd
 fut -l py -o GJGameLevel.py %file% -n gdd
 
